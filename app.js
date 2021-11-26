b34fc9602149618081c8d542f89c40ff157bd853
Agustin Potes <potes_agstin@hotmail.com>
2021-11-25 21:26:03 -0300

    Subo cambios con nueva estructura EJS

--- app.js
+++ app.js
@@ -1,46 +1,14 @@
-const express = require("express");
-const path = require("path");
-
+const express = require('express');
 const app = express();
 
-const publicPath = path.resolve(__dirname, "./public"); 
 
-app.use(express.static(publicPath));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 
+const mainRouter = require('./routes/index');
+app.use('/', mainRouter);
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo en el puerto 3000");
-});
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/index.html"));
-});
-
-app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/register.html"));
-});
-
-app.post("/registerUser", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/index.html"));
-});
-
-app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/login.html"));
-});
-
-app.post("/loginUser", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/index.html"));
-});
-
-app.get("/product", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/product.html"));
-});
-
-app.get("/cart", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/cart.html"));
-});
-
-app.post("/cartUser", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/index.html"));
+app.listen(3000, () => { console.log('Servidor corriendo en el puerto 3000');
 });
