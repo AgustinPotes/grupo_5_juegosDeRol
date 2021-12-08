404d284ee7e4b20800466c217dab0fe67581dfc7
LucioDangelo <ldangelo@itba.edu.ar>
2021-12-07 22:27:38 -0300

    Trabajando en el detail

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,15 +3,20 @@ const path = require('path');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 
 const productController = {
 	index: (req, res) => {
 		res.send('product', {products});
 	},
+	
 	detail: (req, res) => {
-		res.render('detail');
-	},
+        let productDetail = req.params.id;
+		let product = products.find(product => product.id == productDetail)
+        res.render('detail', {product,toThousand});
+    },
+
 	cart: (req, res) => {
 		res.render('cart');
 	},
@@ -24,3 +29,5 @@ const productController = {
 }
 
 module.exports = productController;
+
+	
