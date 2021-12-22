9e8b284c5eed2c7e9a1445057c1b6ef1f469e59a
LucioDangelo <ldangelo@itba.edu.ar>
2021-12-22 01:17:00 -0300

    instalado express validator y creo validaciones de usuario parte 1

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -12,12 +12,21 @@ const storage = multer.diskStorage({
 })
 const upload = multer({storage: storage})
 
+// ************ Express-Validator ************ 
+const { body } = require('express-validator');
+const validaciones = [
+    body('name').notEmpty(),
+    body('lastName').notEmpty(),
+    body('eMail').notEmpty(),
+    body('password').notEmpty(),
+];
+
 const router = express.Router();
 const usersController = require('../controllers/usersController')
 
 
 router.get('/login', usersController.login);
-router.get('/register', usersController.register);
+router.get('/register', validaciones, usersController.register);
 router.post('/register', upload.any(), usersController.newUser);
 
 module.exports = router;
