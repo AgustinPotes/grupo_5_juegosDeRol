83fabd0170ed7e6ffbbdfec30c32ae7be1fbecbd
LucioDangelo <ldangelo@itba.edu.ar>
2021-12-29 02:29:35 -0300

    Script de datos pt2: product, p_categories y p_publishers

---
+++ data/product_categories.sql
@@ -0,0 +1,3 @@
+insert into product_categories (category_id, category_name) values (1, 'RolBoardGame');
+insert into product_categories (category_id, category_name) values (2, 'MiniaturesAndAccesories');
+insert into product_categories (category_id, category_name) values (3, 'TCG');
