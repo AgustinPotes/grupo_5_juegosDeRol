59a179c11e0d2122bcce65572f2a2f7ebe380cf7
Bruno Braconi <brunorbraconi@hotmail.com>
2022-01-14 02:28:26 -0300

    boton de editar producto y ruta de redireccionamiento, middlewares aplicados a rutas de productos

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -3,14 +3,15 @@ const path = require('path')
 const upload = require('../middlewares/multerMiddlewareProduct')
 const router = express.Router();
 const productController = require('../controllers/productsController')
+const authMiddleware = require('../middlewares/authMiddleware')
 
 
 router.get('/', productController.index);
 router.get('/cart', productController.cart);
-router.get('/addProduct', productController.addProduct);
-router.post('/addProduct', upload.any(), productController.newProduct);
-router.get('/editProduct/:id', productController.editProduct); 
-router.put('/editProduct/:id', upload.any(), productController.update); 
+router.get('/addProduct', authMiddleware, productController.addProduct);
+router.post('/addProduct', authMiddleware, upload.any(), productController.newProduct);
+router.get('/editProduct/:id', authMiddleware, productController.editProduct); 
+router.put('/editProduct/:id', authMiddleware, upload.any(), productController.update); 
 router.get('/detail/:id', productController.detail);
 router.delete('/editProduct/:id', productController.delete);
 
