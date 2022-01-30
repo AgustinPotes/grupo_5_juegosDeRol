d67b5cc40803ea1027eea4a05c6caaed74942654
AgustinPotes <potes_agstin@hotmail.com>
2022-01-29 22:22:02 -0300

    Subo arreglos en imagen de perfil y session cuando se está loggeado

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,7 @@ hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('is-active')
     mobile_menu.classList.toggle('is-active')
 });
+
+const userLoggedNewBcg = document.querySelector('.desktop-nav');
+
+userLoggedNewBcg.style.backgroundColor = rgba(149, 147, 255);
