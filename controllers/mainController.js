d6f75a7b998dcc82cab81f9086c36235dcfdd8aa
AgustinPotes <potes_agstin@hotmail.com>
2022-02-10 07:41:01 -0300

    Subo dinámico el index con arreglo de controllador, ruta y apartado visual

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,14 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const db = require('../database/models/index');
+const Product = require("../database/models/Product");
 
 const mainController = {
-
-    index: (req, res) => {
-        res.render('index', {products});
-    },
-};
+        index: async (req, res) => {
+            db.Product.findAll({
+                order: [["title", "ASC"]],
+            })
+               .then(products => {
+                    res.render('index', {products});
+               })
+               .catch(err => {
+                res.send(err)
+    
+            })
+        },
+}
 
 module.exports = mainController;
