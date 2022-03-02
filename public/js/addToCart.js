a75b6907ded7f6a30c88c8de883c08add049b406
Agustin Potes <potes_agstin@hotmail.com>
2022-03-02 19:57:58 -0300

    Funcionalidad del carrito lista en vista cart

---
+++ public/js/addToCart.js
@@ -0,0 +1,50 @@
+window.addEventListener('load', function() {
+
+let itemsInCarrito = JSON.parse(localStorage.arrayCarrito).length;
+
+console.log('itemsInCarrito ' + typeof(itemsInCarrito) + ' ' + itemsInCarrito)
+
+let itemsInCart = document.getElementById('cart-item-counter').innerHTML += parseFloat(itemsInCarrito)
+
+console.log('itemsInCart ' + typeof(itemsInCart) + ' ' + itemsInCart)
+
+
+//selecciona el link del botón para capturar el onclick
+let addToCart = document.querySelector('.add-cart-button');
+
+
+//captura las propiedades que luego querremos mostrar en la vista del carrito
+let title = document.querySelector('.product-name').title;
+let price = document.querySelector('.price-detail').title;
+let image = document.querySelector('.image-product-detail').title;
+
+
+
+
+//crea un array de objetos con las propiedades del producto que acabamos de agregar
+let productsOnCart = [];
+
+addToCart.addEventListener('click', () => {
+        productsOnCart = JSON.parse(localStorage.getItem('arrayCarrito')) || [];
+        let newItem = {
+            title: title,
+            price: price,
+            image: image
+        }
+        
+        console.log('contenido del local : ' + localStorage.getItem('arrayCarrito'))
+
+        console.log('productsOnCart adentro ' + productsOnCart)
+        productsOnCart.push(newItem)
+        localStorage.setItem('arrayCarrito', JSON.stringify(productsOnCart));
+    })
+
+console.log('productsOnCart ' + productsOnCart)
+
+console.log('contenido del local 2: ' + localStorage.getItem('arrayCarrito'))
+
+//captura el span donde se contabilizan los productos en el carrito y lo muestra como número en la nav
+
+
+
+})
--- public/js/cart.js
+++ public/js/cart.js
@@ -1,51 +1,108 @@
 window.addEventListener('load', function() {
 
-//selecciona el link del botón para capturar el onclick
-let addToCart = document.querySelector('.add-cart-button');
 
+//captura las propiedades en la vista del carrito
+/*let titleCart = document.querySelector('.product-name-cart');
+let priceCart = document.querySelector('.price-detail-cart');
+let imageCart = document.getElementById('image-product-cart');
 
-//captura las propiedades que luego querremos mostrar en la vista del carrito
-let title = document.querySelector('.product-name').title;
-let price = document.querySelector('.price-detail').title;
-let image = document.querySelector('.image-product-detail').title;
+let mainProductCartContainers = document.querySelector('.main-product-detail-cart');
 
+console.log('mainProductCartContainers ' + typeof(mainProductCartContainers) + ' ' + mainProductCartContainers)*/
 
-//crea un array de objetos con las propiedades del producto que acabamos de agregar
-let productsOnCart = [];
+/*//captura el span donde se contabilizan los productos en el carrito y lo muestra como número en la nav
 
-addToCart.addEventListener('click', () => {
-        productsOnCart = JSON.parse(localStorage.getItem('arrayCarrito')) || [];
-        let newItem = {
-            title: title,
-            price: price,
-            image: image
-        }
-        
-        console.log('contenido del local : ' + localStorage.getItem('arrayCarrito'))
-
-        console.log('productsOnCart adentro ' + productsOnCart)
-        productsOnCart.push(newItem)
-        localStorage.setItem('arrayCarrito', JSON.stringify(productsOnCart));
-    })
-
-console.log('productsOnCart ' + productsOnCart)
-
-console.log('contenido del local 2: ' + localStorage.getItem('arrayCarrito'))
-
-
-//captura el span donde se contabilizan los productos en el carrito y lo muestra como número en la nav
 let itemsInCarrito = JSON.parse(localStorage.arrayCarrito).length;
 
-console.log('itemsInCarrito ' + typeof(itemsInCarrito) + ' ' + itemsInCarrito)
-
-let itemsInCart = document.getElementsByName('.cart-item-counter').innerHTML = itemsInCarrito;
-
-console.log('itemsInCart ' + typeof(itemsInCart) + ' ' + itemsInCart)
+console.log('itemsInCarrito ' + typeof(itemsInCarrito) + ' ' + itemsInCarrito)*/
 
 
 //Array de productos para ser recorrido
-let arrayProdsDeconstr = JSON.parse(localStorage.arrayCarrito);
+let arrayProds = JSON.parse(localStorage.arrayCarrito);
 
-console.log(arrayProdsDeconstr);
+console.log(arrayProds);
+
+
+//Ciclo for que trae los objetos de cada producto
+/*let forEachProds = arrayProds.forEach(function(arrayProd, i){
+    console.log(arrayProd)
+
+    titleCart.innerHTML += arrayProd.title
+    priceCart.innerHTML += arrayProd.price
+    imageCart.src = arrayProd.image
+})*/
+
+
+
+/*function cicloFor() {for (let x = 0, x < arrayProds.length, x++) {
+    if (arrayProds.length == 0 || []) {
+        console.log('no hay nada en el carrito')
+    } else {
+        mainProductCartContainers.innerHTML += forEachProds
+}
+}}*/
+
+
+
+/*
+arrayProds.forEach(function(arrayProd, i){
+    console.log(arrayProd)
+
+    titleCart.innerHTML += arrayProd.title
+    priceCart.innerHTML += arrayProd.price
+    imageCart.src = arrayProd.image
+})*/
+
+
+
+/*mainProductCartContainers.forEach(function(mainProductCartContainer) {
+    mainProductCartContainer.arrayProds.forEach(function(arrayProd, i){
+        console.log(arrayProd)
+    
+        titleCart.innerHTML += arrayProd.title
+        priceCart.innerHTML += arrayProd.price
+        imageCart.src = arrayProd.image
+    })
+})*/
+
+
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+if (typeof localStorage.arrayCarrito == "undefined" || typeof localStorage.arrayCarrito == "[]") {
+    let div = document.getElementById("main-container-detail");
+    div.innerHTML += "<h2>No hay productos agregados </h2>";
+} else {
+    for (let i = 0; i < arrayProds.length; i++) {
+        let producto = arrayProds[i];
+        let div = document.querySelector(".main-container-detail");
+        let contenido = ` <section class="main-container-detail">
+
+        <div class="main-product-detail-cart">
+            <img class="image-product-cart" src="${producto.image}" alt="" width="100%" height="100%" name="image-product-cart" id="image-product-cart">
+            <h4 class="product-name-cart" name="product-name-cart" >${producto.title}</h4>
+            <p class="price-detail-cart" name="price-detail-cart" >${producto.price}</p>
+    
+                <div class="quantity-and-cart-block">
+                <div class="product-quantity-class">
+                <label for="product-quantity">Cantidad:</label>
+                <input class="p-detail" list="product-quantity1" placeholder="0" id="product-quantity">
+                <datalist id="product-quantity1">
+                        <option value="0">
+                        <option value="1">
+                        <option value="2">
+                        <option value="3">
+                        <option value="4">
+                        <option value="5">
+                </datalist>
+               
+                </div>
+    </section>`;
+
+      div.innerHTML += contenido;
+    }
+  }
+
+
+
 
 })
