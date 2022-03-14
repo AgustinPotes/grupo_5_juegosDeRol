8ee91c0da738ffd811dccc2ac9bcaa5434fd8b98
Bruno Braconi <brunorbraconi@hotmail.com>
2022-03-14 12:24:24 -0300

    validaciones de producto arregladas

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,7 +3,7 @@ const res = require("express/lib/response");
 const db = require('../database/models/index');
 const Product = require("../database/models/Product");
 const { Op } = require("sequelize")
-
+const { validationResult } = require('express-validator')
 /*const { devNull } = require('os');
 const { title } = require('process');
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");*/
@@ -45,17 +45,32 @@ const productController = {
 	},
 	
 	newProduct: (req, res) => {
-		db.Product.create({
-			title: req.body.title,
-			price: parseFloat(req.body.price),
-			image: req.files[0].filename,
-			descrip: req.body.shortDescription,
-			StatusId: req.body.status,
-			CategoryId: req.body.category
-		})
-		res.redirect('/products');
-	},
-
+        const resultValidation = validationResult(req);
+        if (resultValidation.errors.length > 0) {
+            db.Product.findAll()
+            .then (errorss => {
+                return res.render('addproduct', {
+                    errors: resultValidation.mapped(),
+                    oldData: req.body
+                })
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    } 
+        else {
+            db.Product.create({
+                title: req.body.title,
+                price: parseFloat(req.body.price),
+                image: req.files[0].filename,
+                descrip: req.body.descrip,
+                StatusId: req.body.status,
+                CategoryId: req.body.category
+            })
+    
+            res.redirect('/products')
+        }
+    },
 	editProduct: (req, res) => {
 		db.Product.findByPk(req.params.id)
 		.then(product => {
--- middlewares/validationsMiddlewareProducts.js
+++ middlewares/validationsMiddlewareProducts.js
@@ -1,11 +1,11 @@
 const { check } = require('express-validator');
 
-
-const validationsMiddlewareProducts = [
-    check('title').notEmpty().isLength(5).withMessage('Título es obligatorio y de al menos 5 carácteres'),
-    check('shortDescription').notEmpty().isAlphanumeric().isLength(20).withMessage('Descripción es obligatorio y de al menos 20 carácteres'),
-    check('price').notEmpty().isNumeric().withMessage("Solo números permitidos").isLength(2).withMessage('Precio es obligatorio y de al menos 2 cifras'),
-    check('image').notEmpty().isIn([ "PNG", "JPEG", "GIF", "JPG"]).withMessage('Los formatos soportados son solo JPG, JPEG, PNG y GIF'),
+validationsMiddlewareProducts = [
+    check('title').notEmpty().withMessage('Nombre del Producto es un Campo obligatorio').isLength({ min:5}).withMessage('Su Nombre debera tener al menos 5 caracteres'),
+    check('shortDescription').notEmpty().withMessage('Descripcion del Producto es un Campo obligatorio').isLength({ min:20}).withMessage('Debera tener al menos 20 caracteres'),
+    check('price').notEmpty().withMessage('Precio es un Campo obligatorio').isNumeric().withMessage('Su Precio debera ser numerico'),
+    check('category').notEmpty().withMessage('Debes elegir una Categoria'),
+     // check('image').notEmpty().isIn([ "PNG", "JPEG", "GIF", "JPG"]).withMessage('Los formatos soportados son solo JPG, JPEG, PNG y GIF'),
 ];
 
 module.exports = validationsMiddlewareProducts;
