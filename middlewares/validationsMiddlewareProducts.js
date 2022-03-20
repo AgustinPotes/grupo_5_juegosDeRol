46ddd8e94dcf35e4bc921b5a34ad3944b0a38702
Agustin Potes <potes_agstin@hotmail.com>
2022-03-19 22:09:09 -0300

    Intento 527, vamos viendo...

--- middlewares/validationsMiddlewareProducts.js
+++ middlewares/validationsMiddlewareProducts.js
@@ -1,11 +1,28 @@
 const { check } = require('express-validator');
 
 validationsMiddlewareProducts = [
-    //check('title').notEmpty().withMessage('Nombre del Producto es un Campo obligatorio').isLength({ min:5}).withMessage('Su Nombre debera tener al menos 5 caracteres'),
-    //check('shortDescription').notEmpty().withMessage('Descripcion del Producto es un Campo obligatorio').isLength({ min:20}).withMessage('Debera tener al menos 20 caracteres'),
-    //check('price').notEmpty().withMessage('Precio es un Campo obligatorio').isNumeric().withMessage('Su Precio debera ser numerico'),
-    //check('category').notEmpty().withMessage('Debes elegir una Categoria'),
-     // check('image').notEmpty().isIn([ "PNG", "JPEG", "GIF", "JPG"]).withMessage('Los formatos soportados son solo JPG, JPEG, PNG y GIF'),
+    /*check('title').notEmpty().withMessage('Nombre del producto es un campo obligatorio').isLength({ min:3}).withMessage('Nombre debe tener al menos 5 carácteres'),
+    check('shortDescription').notEmpty().withMessage('Descripción del producto es un campo obligatorio').isLength({ min:20}).withMessage('Descripción debe tener al menos 20 carácteres'),
+    check('price').notEmpty().withMessage('Precio del producto es un campo obligatorio').isNumeric().withMessage('El precio debe ser en números'),
+    check('category').notEmpty().withMessage('Tenés que elegir una categoría'),
+    check('image').notEmpty().withMessage('Tenés que subir una imagen').isIn([ '.PNG', '.JPEG', '.JPG']).withMessage('Los formatos soportados son solo JPG, JPEG y PNG'),
+    /*check("image").custom((value, { req }) => {
+
+        let file = req.file;
+
+        if(file){
+
+            let acceptedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
+
+            let fileExtension = path.extname(file.originalname);
+
+            if (!acceptedExtensions.includes(fileExtension)) {
+                throw new Error('Las extensiones de archivo permitidas son ' + acceptedExtensions.join(', ');
+            }
+        }
+
+        return true;
+    }),*/
 ];
 
 module.exports = validationsMiddlewareProducts;
