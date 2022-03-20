ec39510532dc8149cf556806499060637e8a308b
Agustin Potes <potes_agstin@hotmail.com>
2022-03-20 12:14:37 -0300

    Arreglos varios

--- public/js/addToCart.js
+++ public/js/addToCart.js
@@ -10,16 +10,7 @@ window.addEventListener('load', function() {
 }
 };*/
 
-let itemsInCarrito = JSON.parse(localStorage.arrayCarrito).length;
- (itemsInCarrito) =>  itemsInCarrito == undefined || [] || NaN ? itemsInCarrito = 0 : itemsInCarrito = JSON.parse(localStorage.arrayCarrito).length
- 
- //let itemsInCarrito = JSON.parse(localStorage.arrayCarrito).length;
- 
- console.log('itemsInCarrito ' + typeof(itemsInCarrito) + ' ' + itemsInCarrito)
-     
- let itemsInCart = document.getElementById('cart-item-counter').innerHTML += itemsInCarrito
-     
- console.log('itemsInCart ' + typeof(itemsInCart) + ' ' + itemsInCart)
+
  
 
  //selecciona el link del botón para capturar el onclick
@@ -60,5 +51,14 @@ console.log('id ' + ' ' + typeof(id) + ' ' + id)
 
  console.log('contenido del local 2: ' + localStorage.getItem('arrayCarrito'))
 
+ let itemsInCarrito = JSON.parse(localStorage.arrayCarrito).length;
+ (itemsInCarrito) =>  itemsInCarrito == undefined || [] || NaN ? itemsInCarrito = 0 : itemsInCarrito = JSON.parse(localStorage.arrayCarrito).length
  
+ //let itemsInCarrito = JSON.parse(localStorage.arrayCarrito).length;
+ 
+ console.log('itemsInCarrito ' + typeof(itemsInCarrito) + ' ' + itemsInCarrito)
+     
+ let itemsInCart = document.getElementById('cart-item-counter').innerHTML += itemsInCarrito
+     
+ console.log('itemsInCart ' + typeof(itemsInCart) + ' ' + itemsInCart)
  })
