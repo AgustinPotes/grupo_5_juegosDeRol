1962f5f5f730314691bdb6a5ffe3053895e74263
Agustin Potes <potes_agstin@hotmail.com>
2022-03-22 16:27:54 -0300

    Arreglos varios

--- public/js/validatorRegister.js
+++ public/js/validatorRegister.js
@@ -8,7 +8,7 @@
     let errors = [];
     let inputName = document.querySelector('#name');
     if (inputName.value == '') {
-        errors.push('Nombre es un Campo obligatorio')
+        errors.push('Nombre es un campo obligatorio')
     } else if (inputName.value.length < 2) {
         errors.push('El campo Nombre debe tener al menos 2 caracteres')
     }
@@ -16,25 +16,25 @@
 
     let inputLastName = document.querySelector('#lastName');
     if(inputLastName.value == '') {
-        errors.push('Apellido es un Campo obligatorio')
+        errors.push('Apellido es un campo obligatorio')
     } else if (inputLastName.value.length < 2) {
         errors.push('El campo Apellido debe tener al menos 2 caracteres')
     }
 
     let inputUser = document.querySelector('#user');
     if (inputUser.value == '') {
-        errors.push('Usuario es un Campo obligatorio')
+        errors.push('Usuario es un campo obligatorio')
     } 
 
     let inputEmail = document.querySelector('#email');
     if (inputEmail.value == '') {
-        errors.push('Email es un Campo obligatorio')
+        errors.push('Email es un campo obligatorio')
     } 
 
     let inputPassword = document.querySelector('#password');
     if (inputPassword.value == '') {
-        errors.push('Contrasena es un Campo obligatorio')
-    } else if (inputPassword.value.length < 2) {
+        errors.push('Contrasena es un campo obligatorio')
+    } else if (inputPassword.value.length < 8) {
         errors.push('El campo Contrasena debe tener al menos 8 caracteres')
     } else if (inputPassword.value.indexOf(number) == -1) {
         errors.push('El campo Contrasena debe tener al menos un número')
